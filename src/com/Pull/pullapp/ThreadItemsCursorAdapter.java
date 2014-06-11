cce45a3f7ca2de2cb56fa762cadd7c5230163942
lauren7249 <laurentracytalbot@gmail.com>
2014-06-11 18:54:55 -0400

    fixed slow loading for threads list

--- src/com/Pull/pullapp/ThreadItemsCursorAdapter.java
+++ src/com/Pull/pullapp/ThreadItemsCursorAdapter.java
@@ -1,9 +1,9 @@
 package com.Pull.pullapp;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.support.v4.widget.CursorAdapter;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,9 +12,13 @@ import android.widget.TextView;
 import com.Pull.pullapp.util.ContentUtils;
 public class ThreadItemsCursorAdapter extends CursorAdapter {
 	
+	private SharedPreferences mPrefs;
+	private SharedPreferences.Editor mEditor;
     @SuppressWarnings("deprecation")
 	public ThreadItemsCursorAdapter(Context context, Cursor cursor) {
     	super(context, cursor);
+    	mPrefs = context.getSharedPreferences(ThreadItemsCursorAdapter.class.getSimpleName(),Context.MODE_PRIVATE);
+    	mEditor= mPrefs.edit();
     }
 
 	@Override
@@ -26,12 +30,14 @@ public class ThreadItemsCursorAdapter extends CursorAdapter {
     	read = (!threads.getString(1).equals("0"));	
     	recipientId = threads.getString(2);
     	snippet = threads.getString(4);		    		    	
-    	 
-
-		number = ContentUtils.getAddressFromID(context, recipientId);
-		Log.i("recipientId",recipientId);
-		name = ContentUtils.getContactDisplayNameByNumber(context, number);
-
+    	
+    	name = mPrefs.getString(recipientId, null);
+    	if(name==null) {
+			number = ContentUtils.getAddressFromID(context, recipientId);
+			name = ContentUtils.getContactDisplayNameByNumber(context, number);
+			mEditor.putString(recipientId, name);
+			mEditor.commit();
+    	}
 		TextView name_view = (TextView) v.findViewById(R.id.txt_title);
  	   	TextView snippet_view = (TextView) v.findViewById(R.id.txt_message_info);
 
@@ -53,11 +59,14 @@ public class ThreadItemsCursorAdapter extends CursorAdapter {
     	read = (!threads.getString(1).equals("0"));	 
     	recipientId = threads.getString(2);
     	snippet = threads.getString(4);		    		    	
-    	    	
 		
-		number = ContentUtils.getAddressFromID(context, recipientId);
-		Log.i("recipientId",recipientId);
-    	name = ContentUtils.getContactDisplayNameByNumber(context, number);
+    	name = mPrefs.getString(recipientId, null);
+    	if(name==null) {
+			number = ContentUtils.getAddressFromID(context, recipientId);
+			name = ContentUtils.getContactDisplayNameByNumber(context, number);
+			mEditor.putString(recipientId, name);
+			mEditor.commit();
+    	}
 		TextView name_view = (TextView) v.findViewById(R.id.txt_title);
 		TextView snippet_view = (TextView) v.findViewById(R.id.txt_message_info);
 
